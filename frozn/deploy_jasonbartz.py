d83a1e9794fed7cb206d9be3cdd43e55cff74006
Jason Bartz <jrbartz@gmail.com>
2012-01-19 23:09:46 -0500

    added py script for deploying jbartz

---
+++ frozn/deploy_jasonbartz.py
@@ -0,0 +1,6 @@
+from frozn.builder import site
+jasonbartz = site.Site(root='/Users/bartz/Code/repo/blog/frozn',
+            deploy_directory='/Users/bartz/Code/repo/blog-static/frozn/frozn/_deploy/', 
+            site_directory='/Users/bartz/Code/repo/blog-static/frozn/frozn/_site/',
+            config_file='/Users/bartz/Code/repo/blog-static/frozn/frozn/config.json')
+jasonbartz.build()
