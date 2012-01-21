25fe5070bcaceec1ab1f39351ae64749faa05dec
Jason Bartz <jrbartz@gmail.com>
2012-01-21 11:15:51 -0500

    added static

--- frozn/deploy_jasonbartz.py
+++ frozn/deploy_jasonbartz.py
@@ -2,5 +2,6 @@ from frozn.builder import site
 jasonbartz = site.Site(root='/Users/bartz/Code/repo/blog/frozn',
             deploy_directory='/Users/bartz/Code/repo/blog-static/frozn/frozn/_deploy/', 
             site_directory='/Users/bartz/Code/repo/blog-static/frozn/frozn/_site/',
-            config_file='/Users/bartz/Code/repo/blog-static/frozn/frozn/config.json')
+            config_file='/Users/bartz/Code/repo/blog-static/frozn/frozn/config.json',
+            static_files_source='/Users/bartz/Code/repo/blog-static/frozn/frozn/static/')
 jasonbartz.build()
